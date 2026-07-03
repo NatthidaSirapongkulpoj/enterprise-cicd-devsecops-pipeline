c60a4e479b2fa0eefc442c008e60ef0cb3ad8ccf
Natthida Sirapongkulpoj <natthida.sira@gmail.com>
2026-07-03 09:42:23 +0700

    feat: implement sample Flask API

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Config:
+    APP_NAME = "Enterprise CI/CD DevSecOps Pipeline"
+    VERSION = "1.0.0"
+    
---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from flask import jsonify
+from config import Config
+
+
+def register_routes(app):
+
+    @app.route("/")
+    def home():
+        return jsonify({
+            "application": Config.APP_NAME,
+            "version": Config.VERSION,
+            "status": "Running"
+        })
+
+    @app.route("/health")
+    def health():
+        return jsonify({
+            "status": "healthy"
+        })
+
+    @app.route("/version")
+    def version():
+        return jsonify({
+            "version": Config.VERSION
+        })
+    
